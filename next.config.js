22e08145a0e9e79e43538706da423df7414dee24
encrypted mk <142699880+drmukty@users.noreply.github.com>
2026-08-13 09:50:02 -0700

    Add redirects and headers to next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,53 @@ const nextConfig = {
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/scan',
+        destination: '/check-in',
+        permanent: false,
+      },
+      {
+        source: '/events/:id/scan',
+        destination: '/events/:id/check-in',
+        permanent: false,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
